14808499349384af81dcdaa03e43644b091d4585
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2025-05-13 18:18:46 -0300

    implement movable player and basic scene elements
    import animation tools (dotween)
    
    create script for player movement experiments
    
    update main scene using basic elements
    
    update technical details
    
    remove dotween (input.getaxis is enough)
    
    This reverts commit 8a8fad5a26681e85d29c72b0d0e3972631e0fbce.
    
    update build settings
    
    implement player tilting for horizontal motion
    
    implement player motion and control
    
    update player script
    
    add collision manager to account for player motion
    
    update main scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player Reference")]
+    public Rigidbody2D player;
+
+    [Header("Player Settings")]
+    public float speed;
+    public float tiltForce;
+    public float tiltOffset;
+    public Vector2 centerOfMass;
+
+    [Header("Keyboard Input")]
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+
+    private void Awake()
+    {
+        AdjustCenterOfMass(centerOfMass);
+    }
+
+    private void Update()
+    {
+        var sign = GetHorizontalMotion();
+
+        if (sign != 0)
+        {
+            var force = sign * tiltForce * Vector2.right;
+            var point = transform.position + tiltOffset * Vector3.up;
+
+            OnMoveTiltPlayer(force, point);
+        }
+
+        // Para evitar cálculos desnecessários no método MovePlayer()
+        // caso as teclas de movimento estejam inativas:
+        if (!Input.GetKey(KeyCode.None))
+            MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if (!Input.GetKey(up) && !Input.GetKey(down))
+        {
+            transform.Translate(Time.deltaTime * speed * Input.GetAxis("Horizontal") * Vector3.right);
+        }
+        else if (!Input.GetKey(left) && !Input.GetKey(right))
+        {
+            transform.Translate(Time.deltaTime * speed * Input.GetAxis("Vertical") * Vector3.up);
+        }
+    }
+
+    private void OnMoveTiltPlayer(Vector2 tiltForce, Vector3 tiltPoint)
+    {
+        player.AddForceAtPosition(tiltForce, tiltPoint, ForceMode2D.Impulse);
+    }
+
+    private void AdjustCenterOfMass(Vector2 centerPoint)
+    {
+        player.centerOfMass = centerPoint;
+    }
+
+    private int GetHorizontalMotion()
+    {
+        if (Input.GetKeyDown(left))
+            return -1;
+        else if (Input.GetKeyDown(right))
+            return +1;
+        else
+            return +0;
+    }
+}
