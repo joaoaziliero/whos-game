e0cda3daa71aa5822cddb823fb0dd1a2f7aa8e34
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2025-05-13 18:20:59 -0300

    create script for player movement experiments

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Rigidbody2D>().centerOfMass = Vector2.down;
+    }
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1.5f);
+        GetComponent<Rigidbody2D>().AddForceAtPosition(2 * Vector2.right, transform.position + 0.5f * Vector3.up, ForceMode2D.Impulse);
+        transform.DOMoveX(15.0f, 5.0f).SetRelative(true).SetEase(Ease.InOutSine);
+    }
+
+    void Update()
+    {
+
+    }
+}
