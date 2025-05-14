6d08bd68813f729abcc1c938fa53217982fc0f03
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2025-05-14 12:36:19 -0300

    update technical details

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using DG.Tweening;
+//using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,7 +14,7 @@ public class Player : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         GetComponent<Rigidbody2D>().AddForceAtPosition(2 * Vector2.right, transform.position + 0.5f * Vector3.up, ForceMode2D.Impulse);
-        transform.DOMoveX(15.0f, 5.0f).SetRelative(true).SetEase(Ease.InOutSine);
+        //transform.DOMoveX(15.0f, 5.0f).SetRelative(true).SetEase(Ease.InOutSine);
     }
 
     void Update()
