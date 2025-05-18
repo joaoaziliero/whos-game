19bb6e88f1475f05cfed72fdf6652dbb31876dc4
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2025-05-18 07:12:28 -0300

    implement player behaviour on death

--- Assets/Scripts/Player/Health/PlayerHealth.cs
+++ Assets/Scripts/Player/Health/PlayerHealth.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerHealth : HealthManager
 {
     public override void OnDeath()
     {
-
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        transform.rotation = Quaternion.Euler(0, 0, 180);
+        GetComponent<HingeJoint2D>().enabled = false;
     }
 }
