a065b5dd94f6f45a631fb9fea352896322d5e53f
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2025-05-18 04:19:24 -0300

    add health management to characters

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : HealthManager
+{
+    public override void OnDeath()
+    {
+
+    }
+}
