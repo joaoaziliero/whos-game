b3c764ccfea89b5abdb1b3ea1f421b9024079724
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2025-05-23 17:02:03 -0300

    update player hinge manager for general use cases

--- Assets/Scripts/HingeJointManager.cs
+++ Assets/Scripts/HingeJointManager.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class HingeJointManager : MonoBehaviour
 {
     public string obstacleTag;
+    private HingeJoint2D hinge;
+
+    private void Awake()
+    {
+        hinge = GetComponent<HingeJoint2D>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag(obstacleTag))
         {
-            GetComponent<HingeJoint2D>().enabled = false;
+            SetHingeActive(false);
         }
     }
 
@@ -16,7 +22,12 @@ public class HingeJointManager : MonoBehaviour
     {
         if (collision.gameObject.CompareTag(obstacleTag))
         {
-            GetComponent<HingeJoint2D>().enabled = true;
+            SetHingeActive(true);
         }
+    }
+
+    public void SetHingeActive(bool value)
+    {
+        hinge.enabled = value;
     }
 }
